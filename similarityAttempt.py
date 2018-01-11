7ebfee7eae0bbc614013c1cc768e75497dd49afb
hailinlu <34908912+hailinlu@users.noreply.github.com>
2018-01-11 17:56:54 +0800

    add new doc

---
+++ similarityAttempt.py
@@ -0,0 +1,22 @@
+import logging
+logging.basicConfig(format='%(asctime)s : %(levelname)s : %(message)s', level=logging.INFO)
+
+from gensim import corpora, models, similarities
+directory = corpora.Dictionary.load('./tmp/deerwester.dict')
+corpus = corpora.MmCorpus("./tmp/deerwester.mm")
+
+lsi = models.LsiModel(corpus, id2word=directory, num_topics=2)
+doc = "Human computer interaction"
+vec_bow = directory.doc2bow(doc.lower().split())
+vec_lsi = lsi[vec_bow]
+
+# print(vec_lsi)
+
+index = similarities.MatrixSimilarity(lsi[corpus])
+index.save('./tmp/deerwester.index')
+
+sims = index[vec_lsi]
+# print(list(enumerate(sims)))
+
+sims = sorted(enumerate(sims),key=lambda item:-item[1])
+print(sims)
